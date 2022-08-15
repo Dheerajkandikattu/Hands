742871ef79d93427cad9a64423df27c4b9682ff3
Dheerajkandikattu <65445438+Dheerajkandikattu@users.noreply.github.com>
2022-08-15 21:49:18 +0530

    Add files via upload

---
+++ RestJunit-1/src/main/java/com/org/tav/RestJunit1/PatientRecordRepository.java
@@ -0,0 +1,8 @@
+package com.org.tav.RestJunit1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRecordRepository extends JpaRepository<PatientRecord, Long>{
+	
+
+}
---
+++ RestJunit-1/src/main/java/com/org/tav/RestJunit1/RestJunit1Application.java
@@ -0,0 +1,13 @@
+package com.org.tav.RestJunit1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJunit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJunit1Application.class, args);
+	}
+
+}
