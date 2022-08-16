6890b17df784ec4b9089910ff489b048f770256b
Dheerajkandikattu <65445438+Dheerajkandikattu@users.noreply.github.com>
2022-08-16 09:14:18 +0530

    Add files via upload

---
+++ SpringbootDemo1/src/main/java/com/org/tav/SpringbootDemo1/SpringbootDemo1Application.java
@@ -0,0 +1,13 @@
+package com.org.tav.SpringbootDemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemo1Application.class, args);
+	}
+
+}
